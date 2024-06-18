282f5ff217ef19be2b6dfa083386c8c567fa7509
Ever VC <ever.vasquezc27@gmail.com>
2024-06-17 19:58:10 -0600

    :adhesive_bandage: fix(punto_fijo): Quitar un parámetro escrito por error del render

--- calc/views.py
+++ calc/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def punto_fijo(request):
-    return render(request, 'punto_fijo.html', name='punto_fijo')
+    return render(request, 'punto_fijo.html')
