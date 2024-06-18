646e8b9bdbaacc29950bb9f7f503a0886d55279b
Ever VC <ever.vasquezc27@gmail.com>
2024-06-17 19:29:26 -0600

    :ok_hand:  feat: Actualización de la ruta a la página de punto fijo en el layout base

--- calc/views.py
+++ calc/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def punto_fijo(request):
-    return render(request, 'punto_fijo.html')
+    return render(request, 'punto_fijo.html', name='punto_fijo')
