6484b6db59795045d3cc4ee36de504aeed93542c
Ever VC <ever.vasquezc27@gmail.com>
2024-06-18 21:40:35 -0600

    :sparkles: feat: Función de registro de usuarios

--- user/views.py
+++ user/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password1')
+        confirm_password = request.POST.get('password2')
+        
+        if password == confirm_password:
+            try:
+                user = User.objects.create_user(username, email, password)
+                user.save()
+                login(request, user)
+                return render(request, 'index.html', {'message': 'Usuario creado con éxito.'})
+            except:
+                return render(request, 'signup.html', {'error': 'El usuario ya existe.'})
