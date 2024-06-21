4ce05552d54d3d24fb6816c02bd04d9aefa01e6c
Ever VC <ever.vasquezc27@gmail.com>
2024-06-21 10:40:56 -0600

    :bricks: feat: Archivo index.html ahora en la app calc, intento de solución de operaciones básicas

--- calc/urls.py
+++ calc/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='home'),
+    path('home/', views.index, name='home'),
     path('punto-fijo/', views.punto_fijo, name='punto_fijo'),
 ]
--- calc/views.py
+++ calc/views.py
@@ -1,5 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import re # Regular expressions
 
 # Create your views here.
+def index(request):
+    if request.method == 'POST':
+        print(request.POST)
+        basic_operation = request.POST.get('basic_operation')
+
+        # Valida de que la operación solo contiene números y operadores básicos
+        if re.match(r'^\d+(\s*[\+\-\*/]\s*\d+)*$', basic_operation):
+            try:
+                result = eval(basic_operation)
+            except ZeroDivisionError:
+                result = "Error: División por cero"
+                return render(request, 'index.html', {'error': result})
+            except Exception as e:
+                result = f"Error: {e}"
+                return render(request, 'index.html', {'error': result})
+        else:
+            result = "Error: Operación no válida"
+            return render(request, 'index.html', {'error': result})
+
+        return render(request, 'index.html', {'result': result})
+    return render(request, 'index.html')
+
+def basic_calc(request):
+    if request.method == 'POST':
+        print(request.POST)
+        basic_operation = request.POST.get('basic_operation')
+
+        # Valida de que la operación solo contiene números y operadores básicos
+        if re.match(r'^\d+(\s*[\+\-\*/]\s*\d+)*$', basic_operation):
+            try:
+                result = eval(basic_operation)
+            except ZeroDivisionError:
+                result = "Error: División por cero"
+                return render(request, '/', {'error': result})
+            except Exception as e:
+                result = f"Error: {e}"
+                return render(request, '/', {'error': result})
+        else:
+            result = "Error: Operación no válida"
+            return render(request, '/', {'error': result})
+
+        return render(request, '/', {'result': result}) # Redirige a la página principal con el resultado
+    else:
+        return redirect('/')
+
+
 def punto_fijo(request):
     return render(request, 'punto_fijo.html')
