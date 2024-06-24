e43bc845aee68de4e7fb6b625a2fb838c7110ef8
Ever VC <ever.vasquezc27@gmail.com>
2024-06-24 03:44:38 -0600

    :wrench: chore(settings): COnfiguración para conectarse a PostgreSQL

--- djangocalc/settings.py
+++ djangocalc/settings.py
@@ -77,11 +77,23 @@ WSGI_APPLICATION = 'djangocalc.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+"""
+# Configuración de la base de datos para PostgreSQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_djangocalc',
+        'USER': 'postgres',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
